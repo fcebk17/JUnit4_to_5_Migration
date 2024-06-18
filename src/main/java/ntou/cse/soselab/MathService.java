c7db55ebd0e8fc05ccdd698c3ed7c015397fc499
Tzuyu <fcebk17@gmail.com>
2024-06-18 23:07:47 +0800

    fix: MathService function

--- src/main/java/ntou/cse/soselab/MathService.java
+++ src/main/java/ntou/cse/soselab/MathService.java
@@ -12,4 +12,8 @@ public class MathService {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int multiply(int i, int i1) {
+        return i * i1;
+    }
 }
--- src/test/java/ntou/cse/soselab/MathServiceTest.java
+++ src/test/java/ntou/cse/soselab/MathServiceTest.java
@@ -1,14 +1,14 @@
 package ntou.cse.soselab;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MathServiceTest {
 
@@ -26,4 +26,11 @@ public class MathServiceTest {
         int result = mathService.subtract(20, 10);
         assertEquals(10, result);
     }
+
+    @Test
+    public void testMultiply() {
+        int result = mathService.multiply(3, 4);
+        assertEquals(12, result);
+    }
+
 }
