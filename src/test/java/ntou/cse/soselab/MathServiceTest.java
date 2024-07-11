1b6181d4c2d4ad4205e32e052339cf94be1cc9d6
Tzuyu <fcebk17@gmail.com>
2024-07-11 09:10:44 +0800

    feat: add other annotation

--- src/test/java/ntou/cse/soselab/MathServiceTest.java
+++ src/test/java/ntou/cse/soselab/MathServiceTest.java
@@ -2,13 +2,25 @@ package ntou.cse.soselab;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 public class MathServiceTest {
 
-    private MathService mathService;
+    private static MathService mathService; // 必須為 static
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // 這個方法會在所有測試方法執行之前執行，且只會執行一次
+        System.out.println("Executing setUpBeforeClass");
+        mathService = new MathService();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        // 這個方法會在所有測試方法執行之後執行，且只會執行一次
+        System.out.println("Executing tearDownAfterClass");
+        mathService = null;
+    }
 
     @Before
     public void setUp() {
